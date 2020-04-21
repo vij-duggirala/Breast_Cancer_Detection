588f4d8b92a68fcd6d6b8c4a935832b38d764c89
vij-duggirala <49618259+vij-duggirala@users.noreply.github.com>
2020-04-21 22:57:56 +0530

    using existing libraries

--- Breast_Cancer_using KNN and SVM.py
+++ Breast_Cancer_using KNN and SVM.py
@@ -2,16 +2,12 @@
 # coding: utf-8
 
 # In[84]:
-
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import sklearn
 
-
 # In[85]:
-
 
 from sklearn import preprocessing
 from sklearn.neighbors import KNeighborsClassifier
@@ -20,9 +16,7 @@ from sklearn.svm import SVC
 from sklearn.metrics import classification_report , accuracy_score
 from pandas.plotting import scatter_matrix
 
-
 # In[86]:
-
 
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data"
 names = ['id','clump_thickness', 'uniform_cell_size', 'uniform_cell_shape',
@@ -31,61 +25,36 @@ names = ['id','clump_thickness', 'uniform_cell_size', 'uniform_cell_shape',
 
 df = pd.read_csv(url, names=names)
 
-
 # In[87]:
 
 
 df.replace('?' ,-99999 , inplace =True)
-
-
-# In[88]:
-
-
 print(df.axes)
 df.drop('id' , 1, inplace=True)
 print(df.shape)
 
-
 # In[89]:
 
-
 df.describe()
-
-
-
-
-
 df.head()
-
-
-
-
 print(df.loc[99])
 
-
 # In[37]:
-
 
 df.hist(figsize = (8,8))
 plt.show()
 
-
 # In[94]:
-
 
 scatter_matrix(df , figsize = (20,20))
 plt.show()
 
-
 # In[96]:
-
 
 y = np.array(df['class'])
 X = np.array(df.drop(['class'] , 1))
 
 from sklearn.model_selection import train_test_split
-
-
 X_train, X_test , y_train , y_test = train_test_split(X, y , test_size = 0.2)
 
 models = []
